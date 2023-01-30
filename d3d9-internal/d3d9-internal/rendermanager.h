975f4a5fc6b606ebdecd9f07f1c32cd81ce276ee
21Dogs <48073843+21Dogs@users.noreply.github.com>
2023-01-30 00:30:17 +0000

    Added draw line

--- d3d9-internal/d3d9-internal/rendermanager.h
+++ d3d9-internal/d3d9-internal/rendermanager.h
@@ -8,6 +8,7 @@ class CRenderManager {
 public:
     IDirect3DDevice9* device;
     ID3DXFont* font;
+    ID3DXLine* line;
 
     void initialise() {
         // Get the Direct3D device
@@ -15,11 +16,21 @@ public:
 
         //create the font object
         D3DXCreateFont(device, 20, 0, FW_BOLD, 1, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, ANTIALIASED_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"Arial", &font);
+
+        //create the line object
+        D3DXCreateLine(device, &line);
     }
 
     void DrawText(int x, int y, DWORD color, const char* text) {
         RECT rect = { x, y, x + 100, y + 20 };
         font->DrawTextA(NULL, text, -1, &rect, DT_NOCLIP, color);
+    }
+
+    void DrawLine(int x1, int y1, int x2, int y2, DWORD color) {
+        D3DXVECTOR2 points[2] = { D3DXVECTOR2((float)x1, (float)y1), D3DXVECTOR2((float)x2, (float)y2) };
+        line->Begin();
+        line->Draw(points, 2, color);
+        line->End();
     }
 };
 
